f1a98efdfb4033b2167d662be43de8d37d074105
Tibbelit <tibbelit@gmail.com>
2019-05-06 15:39:39 +0200

    Code from lecture

---
+++ app/Http/Controllers/MoviesController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Movie;
+use Illuminate\Http\Request;
+
+class MoviesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $movies = Movie::all();
+        return view('movies.index', [
+            'movies' => $movies
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('movies.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $movie = new Movie;
+        $movie->title = $request->input('title');
+        $movie->year = $request->input('year');
+        $movie->poster = $request->input('poster');
+        $movie->save();
+
+        return redirect()->route('movies.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $movie = Movie::find($id);
+        return view('movies.show', [
+            'movie' => $movie
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $movie = Movie::find($id);
+        return view('movies.edit', [
+            'movie' => $movie
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $movie = Movie::find($id);
+        $movie->title = $request->input('title');
+        $movie->year = $request->input('year');
+        $movie->poster = $request->input('poster');
+        $movie->save();
+
+        return redirect()->route('movies.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Movie::destroy($id);
+
+        return redirect()->route('movies.index');
+    }
+}
